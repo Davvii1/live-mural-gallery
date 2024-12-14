02d1e45fdafeaef46323238154eede67b60367c1
Davvii <david2002rg@hotmail.com>
2024-12-13 21:02:43 -0600

    fix: auth inside provider

--- components/AblyWrapper.tsx
+++ components/AblyWrapper.tsx
@@ -4,11 +4,12 @@ import Ably from 'ably';
 import { ReactNode } from 'react';
 import { AblyProvider, ChannelProvider } from 'ably/react';
 
-const ably = new Ably.Realtime({
-    authUrl: "https://v0-live-mural-gallery-q7dtqa5vvli.vercel.app/api/token",
-});
 
 export const AblyWrapper = ({ children }: { children: ReactNode }) => {
+    const ably = new Ably.Realtime({
+        authUrl: "https://v0-live-mural-gallery-q7dtqa5vvli.vercel.app/api/token",
+    });
+    
     return (
         <AblyProvider client={ably}>
             <ChannelProvider channelName='photos'>
