28a966c853fa15fd7d07b5e800e0b188003708e2
Davvii <david2002rg@hotmail.com>
2024-12-13 20:49:48 -0600

    feat: remove query time from client

--- components/AblyWrapper.tsx
+++ components/AblyWrapper.tsx
@@ -4,7 +4,9 @@ import Ably from 'ably';
 import { ReactNode } from 'react';
 import { AblyProvider, ChannelProvider } from 'ably/react';
 
-const ably = new Ably.Realtime({ authUrl: "https://v0-live-mural-gallery-q7dtqa5vvli.vercel.app/api/token", queryTime: true });
+const ably = new Ably.Realtime({
+    authUrl: "https://v0-live-mural-gallery-q7dtqa5vvli.vercel.app/api/token",
+});
 
 export const AblyWrapper = ({ children }: { children: ReactNode }) => {
     return (
