57d0816ddcb558439f3a5e407b684bff95b28684
Davvii <david2002rg@hotmail.com>
2024-12-13 21:28:17 -0600

    fix: App component client

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 import App from "../App"
-import { useChannel } from "ably/react";
 import Ably from 'ably';
 import { AblyProvider, ChannelProvider } from 'ably/react';
-
-export const revalidate = 0
 
 const ably = new Ably.Realtime({
   authUrl: "https://v0-live-mural-gallery-q7dtqa5vvli.vercel.app/api/token",
 });
 
-export default async function SyntheticV0PageForDeployment() {
+export default function SyntheticV0PageForDeployment() {
   return (
     <AblyProvider client={ably}>
       <ChannelProvider channelName='photos'>
