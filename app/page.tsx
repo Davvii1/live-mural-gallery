dcd2338927c95927ea2ef6b384b9e5e29b271f80
Davvii <david2002rg@hotmail.com>
2024-12-14 00:12:26 -0600

    fix: ably inside page return

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import App from "../App"
 import Ably from 'ably';
 import { AblyProvider, ChannelProvider } from 'ably/react';
 
-
 export default function SyntheticV0PageForDeployment() {
   const ably = new Ably.Realtime({
     authUrl: "https://v0-live-mural-gallery-q7dtqa5vvli.vercel.app/api/token",
+    clientId: "*"
   });
 
   return (
