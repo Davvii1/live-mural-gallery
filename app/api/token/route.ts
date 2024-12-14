c30078c4b01c858d9e8beed234fbc7003a43ceeb
Davvii <david2002rg@hotmail.com>
2024-12-14 09:37:31 -0600

    fix: revalidate 0 from server

--- app/api/token/route.ts
+++ app/api/token/route.ts
@@ -1,15 +1,14 @@
 import * as Ably from "ably";
 
+export const revalidate = 0;
+
 export async function GET(request: Request) {
   const ably = new Ably.Rest({
     key: process.env.ABLY_API_KEY,
     queryTime: true,
-    clientId: "mural",
   });
-
   const tokenRequest = await ably.auth.createTokenRequest({
-    clientId: "mural",
+    clientId: "live-gallery-mural",
   });
-
   return Response.json(tokenRequest);
 }
