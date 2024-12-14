d25518a61138164faf69069e7428de2cf13472d1
Davvii <david2002rg@hotmail.com>
2024-12-14 09:49:22 -0600

    fix: change upload endpoint

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -4,17 +4,13 @@ export async function POST(request: Request) {
   const ably = new Ably.Rest({
     key: process.env.ABLY_API_KEY,
     queryTime: true,
-    clientId: "mural",
   });
 
   const data = await request.json();
 
   const photosChannel = ably.channels.get("photos");
 
-  await photosChannel.publish(
-    "photoUploaded",
-    JSON.stringify({ file: data.file })
-  );
+  await photosChannel.publish("photoUploaded", data);
 
   return Response.json({ message: "Message sent!" });
 }
