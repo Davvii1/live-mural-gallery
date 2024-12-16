b26c4fd975903714b760d53ffe7cce4e36b8e963
Davvii <david2002rg@hotmail.com>
2024-12-16 16:17:04 -0600

    feat: add qr code to upload

--- components/ImagePlaceholderButton.tsx
+++ components/ImagePlaceholderButton.tsx
@@ -1,18 +1,29 @@
-import { Image } from 'lucide-react'
+'use client'
+import clsx from "clsx"
+import Image from "next/image"
+import { useState } from "react"
 
 interface ImagePlaceholderButtonProps {
   onNewImage: () => void
 }
 
 export function ImagePlaceholderButton({ onNewImage }: ImagePlaceholderButtonProps) {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <button
-      className="fixed bottom-4 left-1/2 -translate-x-1/2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full shadow-lg transition-colors duration-200 flex items-center space-x-2"
-      onClick={onNewImage}
+      className={clsx('fixed bottom-6 right-12 bg-white font-bold py-2 px-4 rounded-md shadow-lg transition-all duration-200 flex items-center space-x-2', {
+        'flex-col': isOpen,
+      })}
+      onClick={() => setIsOpen(!isOpen)}
     >
-      <Image className="w-6 h-6" />
-      <span>Add New Image</span>
+      <div className={clsx('aspect-square relative', {
+        'w-12': !isOpen,
+        'w-full': isOpen,
+      })}>
+        <Image src="/uploadqr.png" alt="QR Code" fill />
+      </div>
+      <span>Agrega tu imagen</span>
     </button>
   )
 }
-
